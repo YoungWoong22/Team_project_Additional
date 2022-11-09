c57b0f6e75b7443bb161828fb1bac99b1a5ddd4c
YoungWoong22 <hwa8078678@naver.com>
2022-11-09 23:55:06 +0900

    팀프로젝트였는데 기간이끝나서 혼자 더 디자인하고 기능을 추가 구현할 생각이다.

--- README.md
+++
@@ -1,2 +0,0 @@
-# mvc-20220927-leeyw
-[계] 자바기반 백엔드 개발자 양성 과정 springboot mvc
--- src/main/java/com/study/mvc20220927leeyw/service/TestService.java
+++
@@ -1,8 +0,0 @@
-package com.study.mvc20220927leeyw.service;
-
-public interface TestService {
-
-    public void logic1();
-    public void logic2();
-
-}
